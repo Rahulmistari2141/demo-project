a2c6d3ecd38a855d7ed2c19946a65ef3bc1f0415
Rahulmistari2141 <rahulmistari2141@gmail.com>
2026-02-03 10:21:47 +0530

    changes

--- src/app/core/auth/auth-guard-guard.ts
+++ src/app/core/auth/auth-guard-guard.ts
@@ -2,7 +2,6 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuardGuard: CanActivateFn = (route, state) => {
-  debugger;
   const router = inject(Router);
   const localData = localStorage.getItem('batchUser');
   if(localData != null){
